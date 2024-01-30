2768ab698cc4e1662374e9710fd1e7f2c38463be
Jumpei Yamaoka <junpeiyamaoka0516@gmail.com>
2024-01-30 22:00:13 +0900

    update: api requestを他のform requestにも反映

--- html/message-board/app/Http/Requests/StoreThreadRequest.php
+++ html/message-board/app/Http/Requests/StoreThreadRequest.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
-
-class StoreThreadRequest extends FormRequest
+class StoreThreadRequest extends ApiRequest
 {
     /**
      * Determine if the user is authorized to make this request.
--- html/message-board/app/Http/Requests/UpdateThreadRequest.php
+++ html/message-board/app/Http/Requests/UpdateThreadRequest.php
@@ -2,9 +2,7 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
-
-class UpdateThreadRequest extends FormRequest
+class UpdateThreadRequest extends ApiRequest
 {
     /**
      * Determine if the user is authorized to make this request.
