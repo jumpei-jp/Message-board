f25053af717ed6088ad6c59f3b24996062f43777
Jumpei Yamaoka <junpeiyamaoka0516@gmail.com>
2024-02-02 23:12:12 +0900

    feat: 投稿一覧から投稿詳細に遷移

--- front/src/components/Thread/Thread.tsx
+++ front/src/components/Thread/Thread.tsx
@@ -1,7 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 interface ThreadProps {
   thread: {
+    id: number
     user_id: number
     title: string
     created_at: string
@@ -14,7 +16,7 @@ const Thread = ({ thread }: ThreadProps) => {
     <li>
         <div className="px-4 shadow py-5 sm:px-6">
             <div className="flex items-center justify-between">
-                <h3 className="text-lg leading-6 font-medium text-gray-900">{thread.title}</h3>
+                <h3 className="text-lg leading-6 font-medium text-gray-900"><Link to={`/threads/${thread.id}`}>{thread.title}</Link></h3>
                 <p className="mt-1 max-w-2xl text-sm text-gray-500">投稿者: {thread.user_id}</p>
             </div>
             <div className="mt-4 items-center justify-between">
